8bc5a50892c29d2725cb53441de4284a38d30aa8
andrsmllr <andrsmllr@bananatronics.org>
2019-10-01 19:39:12 +0200

    Change mode of install script (u+x) and fix removal of tar files

--- install_sifive_toolchain.sh
+++ install_sifive_toolchain.sh
@@ -8,13 +8,13 @@ apt install -qq -y make git gcc
 RISCV_TOOLCHAIN=riscv64-unknown-elf-gcc-8.2.0-2019.05.3-x86_64-linux-ubuntu14
 wget https://static.dev.sifive.com/dev-tools/${RISCV_TOOLCHAIN}.tar.gz
 tar -xf ./${RISCV_TOOLCHAIN}.tar.gz
-rm -f ./${RISCV_TOOLCHAIN}.tar.gz
+rm -f ./${RISCV_TOOLCHAIN}.tar.gz*
 export RISCV_PATH=./${RISCV_TOOLCHAIN}
 
 OPENOCD_DEBUGGER=riscv-openocd-0.10.0-2019.05.1-x86_64-linux-ubuntu14
 wget https://static.dev.sifive.com/dev-tools/${OPENOCD_DEBUGGER}.tar.gz
 tar -xf ./${OPENOCD_DEBUGGER}.tar.gz
-rm -f ./${OPENOCD_DEBUGGER}.tar.gz
+rm -f ./${OPENOCD_DEBUGGER}.tar.gz*
 export OPENOCD_PATH=./${OPENOCD_DEBUGGER}
 
 cd ./freedom-e-sdk
